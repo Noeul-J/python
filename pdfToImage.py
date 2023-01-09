156f34119a47a042f6f7e64650184cfa0159313f
HJ_Jung <60030136+Noeul-J@users.noreply.github.com>
2023-01-09 13:34:23 +0900

    변수명, 함수명 변경

--- pdfToImage.py
+++ pdfToImage.py
@@ -19,16 +19,18 @@ from PIL import Image
 from pdf2image import convert_from_path
 import sys
 
-def save_Image(saveImgPath, assetId):
+
+def save_image(save_img_path, asset_id):
     # file = "C:\RPA\SCN1_Macquarie\OUTPUT\\221230\\7609\\7609.pdf"
-    file = os.path.join(saveImgPath, assetId + "_01.pdf")
+    file = os.path.join(save_img_path, asset_id + "_01.pdf")
+    print(file)
 
     # open the file
     pdf_file = fitz.open(file)
-    images = convert_from_path(file)
+    # images = convert_from_path(file)
 
-    img_save_start = False
-    img_count = 1;
+    # img_save_start = False
+    img_count = 2
 
     # PDF page 만큼 반복
     for page_index in range(len(pdf_file)):
@@ -37,32 +39,31 @@ def save_Image(saveImgPath, assetId):
         image_list = pdf_file.get_page_images(page_index)
 
         if image_list:
-            print(f"[+] Found a total of {len(image_list)} images in page {page_index+1}")
+            print(f"[+] Found a total of {len(image_list)} images in page {page_index + 1}")
 
             for image_index, img in enumerate(image_list, start=1):
                 xref = img[0]
                 base_image = pdf_file.extract_image(xref)
                 image_bytes = base_image["image"]
-                image_ext = base_image["ext"]
                 image = Image.open(io.BytesIO(image_bytes))
                 if img_count < 10:
-                    img_count = '0'+str(img_count)
-                image.save(f"{saveImgPath}{assetId}_{img_count}.jpeg", "jpeg")
+                    img_count = '0' + str(img_count)
+                image.save(f"{save_img_path}{asset_id}_{img_count}.jpeg", "jpeg")
                 img_count = int(img_count) + 1
                 # img_save_start = True
         else:
-            print("[!] No images found on page", (page_index+1))
+            print("[!] No images found on page", (page_index + 1))
             # if not img_save_start :
-            #     images[page_index].save(f"{saveImgPath}{assetId}_{page_index}.jpeg", "jpeg")
+            #     images[page_index].save(f"{saveImgPath}{assetId}_{img_count}.jpeg", "jpeg")
+            # img_count = int(img_count) + 1
 
 
 if __name__ == '__main__':
-    # assetId = sys.argv[1]
-    # saveImgPath = sys.argv[2]
+    asset_id_from_wd = sys.argv[1]
+    save_img_path_from_wd = sys.argv[2]
 
-    saveImgPath = 'C:\RPA\\TEST\\'
-    assetId = '236594'
+    # save_img_path_from_wd = 'C:\\RPA\\TEST\\'
+    # asset_id_from_wd = '204911'
 
-    save_Image(saveImgPath, assetId)
-    print("이미지 저장 완료")
-
+    save_image(save_img_path_from_wd, asset_id_from_wd)
+    print("이미지"" 저장 완료")
